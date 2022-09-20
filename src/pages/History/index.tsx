1b1afb8f3a0814653f544a4fd87921777e71ff78
andrewReis <andrew-reii@outlook.com>
2022-09-20 15:46:36 -0300

    page history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,49 @@
+import { Container, HistoryList, Status } from './styles'
+
 export function History() {
-  return <div>hystory</div>
+  return (
+    <Container>
+      <h1>Meu Historico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="red">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="yellow">Concluido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </Container>
+  )
 }
